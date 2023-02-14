845f43e218e3d4027c5e4d1e84f85b109adb2c25
TrevisGreen <Trevis.Green@gmail.com>
2023-02-14 06:51:42 -0600

    Initial commit

---
+++ src/TextEditor.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextEditor extends JFrame implements ActionListener {
+
+    JTextArea textArea;
+
+    TextEditor() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Text Editor");
+        this.setSize(500,500);
+        this.setLayout(new FlowLayout());
+        this.setLocationRelativeTo(null);
+
+        textArea = new JTextArea();
+        textArea.setPreferredSize(new Dimension(450,450));
+
+        this.add(textArea);
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
