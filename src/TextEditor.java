4d05849ceead0f610974f103665f17bcf2c2aa15
TrevisGreen <Trevis.Green@gmail.com>
2023-02-14 07:06:58 -0600

    Minor updates

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,6 +8,9 @@ import java.awt.event.ActionListener;
 public class TextEditor extends JFrame implements ActionListener {
 
     JTextArea textArea;
+    JScrollPane scrollPane;
+    JSpinner fontSizeSpinner;
+    JLabel fontLabel;
 
     TextEditor() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,9 +20,30 @@ public class TextEditor extends JFrame implements ActionListener {
         this.setLocationRelativeTo(null);
 
         textArea = new JTextArea();
-        textArea.setPreferredSize(new Dimension(450,450));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(new Font("Arial", Font.PLAIN,20));
 
-        this.add(textArea);
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(450,450));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        fontLabel = new JLabel("Font: ");
+
+        fontSizeSpinner = new JSpinner();
+        fontSizeSpinner.setPreferredSize(new Dimension(50,25));
+        fontSizeSpinner.setValue(20);
+        fontSizeSpinner.addChangeListener(new ChangeListener() {
+
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                textArea.setFont(new Font(textArea.getFont().getFamily(), Font.PLAIN, (int)fontSizeSpinner.getValue()));
+            }
+        });
+
+        this.add(fontLabel);
+        this.add(fontSizeSpinner);
+        this.add(scrollPane);
         this.setVisible(true);
     }
 
