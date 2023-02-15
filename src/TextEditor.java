b5f806f190dd6010269ca47bb9fb0e62f4eaf519
TrevisGreen <Trevis.Green@gmail.com>
2023-02-15 10:00:01 -0600

    Made major updates. I have the program up and running.

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,9 +1,14 @@
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class TextEditor extends JFrame implements ActionListener {
 
@@ -11,6 +16,13 @@ public class TextEditor extends JFrame implements ActionListener {
     JScrollPane scrollPane;
     JSpinner fontSizeSpinner;
     JLabel fontLabel;
+    JButton fontColorButton;
+    JComboBox fontBox;
+    JMenuBar menuBar;
+    JMenu fileMenu;
+    JMenuItem openItem;
+    JMenuItem saveItem;
+    JMenuItem exitItem;
 
     TextEditor() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -41,14 +53,114 @@ public class TextEditor extends JFrame implements ActionListener {
             }
         });
 
+        fontColorButton = new JButton("Color");
+        fontColorButton.addActionListener(this);
+
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        fontBox = new JComboBox(fonts);
+        fontBox.addActionListener(this);
+        fontBox.setSelectedItem("Arial");
+
+        // -----------Start of Menubar-----------
+
+        menuBar = new JMenuBar();
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+
+        openItem.addActionListener(this);
+        saveItem.addActionListener(this);
+        exitItem.addActionListener(this);
+
+        fileMenu.add(openItem);
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        menuBar.add(fileMenu);
+
+
+
+        // -----------End of Menubar-----------
+
+        this.setJMenuBar(menuBar);
         this.add(fontLabel);
         this.add(fontSizeSpinner);
+        this.add(fontColorButton);
+        this.add(fontBox);
         this.add(scrollPane);
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == fontColorButton) {
+            JColorChooser colorChooser = new JColorChooser();
 
+            Color  color = colorChooser.showDialog(null,"Choose a color",Color.black);
+
+            textArea.setForeground(color);
+        }
+
+        if(e.getSource() == fontBox) {
+            textArea.setFont(new Font((String)fontBox.getSelectedItem(),Font.PLAIN, textArea.getFont().getSize()));
+        }
+
+        if(e.getSource() == openItem) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text files", "txt");
+            fileChooser.setFileFilter(filter);
+
+            int response = fileChooser.showOpenDialog(null);
+
+            if(response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                Scanner fileIn = null;
+                try {
+                    fileIn = new Scanner(file);
+                    if(file.isFile()) {
+                        while (fileIn.hasNextLine()) {
+                            String line = fileIn.nextLine() + "\n";
+                            textArea.append(line);
+                        }
+                    }
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+                finally {
+                    fileIn.close();
+                }
+            }
+        }
+
+        if(e.getSource() == saveItem) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File("."));
+
+            int response = fileChooser.showSaveDialog(null);
+
+            // int value will return 0 or 1, but better way of writing is:
+            if(response == JFileChooser.APPROVE_OPTION) {
+                File file;
+                PrintWriter fileOut = null;
+
+                file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                try {
+                    fileOut = new PrintWriter(file);
+                    fileOut.println((textArea.getText()));
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+                finally {
+                    fileOut.close();
+                }
+            }
+        }
+
+        if(e.getSource() == exitItem) {
+            System.exit(0);
+        }
     }
 }
